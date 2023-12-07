8a78b1d693a4857c20736dd25f797b546d27ba69
pawara101 <pawara101dassanayake@gmail.com>
2023-12-07 22:18:45 +0530

    Clouda Task v1.0.3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState, useEffect } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 function App() {
-
   const url = "https://reqres.in/api/users";
   const [userData, setUserData] = useState([]);
 
@@ -17,21 +17,46 @@ function App() {
   };
 
   useEffect((data) => {
-    console.log('Fetching data...');
+    console.log("Fetching data...");
     fetchInfo();
   }, []);
 
-
+  const handleItemClick = (userId) => {
+    // Handle the click event, for example, navigate to a user details page
+    console.log(`Clicked on user with ID ${userId}`);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>User Details</h1>
-
+        <table class="usertable">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Email</th>
+              <th></th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              
+            </tr>
+          </thead>
+          <tbody>
+            {userData.map((user) => (
+              <tr key={user.id} onClick={() => handleItemClick(user.id)} style={{ cursor: 'pointer' }}>
+                <td>{user.id}</td>
+                <td>{user.email}</td>
+                <td><img src={user.avatar} class="avatar"></img></td>
+                <td>{user.first_name}</td>
+                <td>{user.last_name}</td>
+                
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </header>
     </div>
   );
-  
 }
 
 export default App;
